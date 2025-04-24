a1290b464d1d68eabc90eb45c71ba60b8e0cb150
Camilo <camilomedina.es@gmail.com>
2025-04-23 22:37:13 -0400

    primer commit

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import config from "./config/config.js";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import passport from "passport";
+import initializePassport from "./config/passport.config.js";
+
+const PORT = config.port;
+const MONGO_URL = config.mongoUrl;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+initializePassport();
+app.use(passport.initialize());
+
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`listening on port ${PORT}`);
+      console.log("Connected to MongoDB");
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to connect to MongoDB:", error);
+  });
